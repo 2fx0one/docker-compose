db2b873ba9a44b5e82402e17bde856c21de6732c
2fx0one <2fx0one@gmail.com>
2019-09-05 15:01:57 +0800

    dockerfile

--- jdk1.8/build.sh
+++ jdk1.8/build.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-docker -H tcp://192.168.199.210:2375 build -t'jdk1.8:0.0.1' .
+
+docker -H tcp://192.168.199.12:2375 build -t'jre1.8:0.0.1' -f Dockerfile.jre .
+docker -H tcp://192.168.199.12:2375 build -t'jdk1.8:0.0.1' -f Dockerfile.jdk .
+
